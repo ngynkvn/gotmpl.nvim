1b308fe160e74905f2f79dac85df2d7b415a3370
Kevin <ngynkvn@gmail.com>
2024-09-21 15:21:42 -0400

    refactor: Move query to queries folder

--- lua/gotmpl/init.lua
+++ lua/gotmpl/init.lua
@@ -1,11 +1,5 @@
 local M = {}
 
-local DEFAULT_INJECTION = [[
-((text) @injection.content
-  (#inject-go-tmpl!)
-  (#set! injection.combined))
-]]
-
 M.setup = function()
 	vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
 		local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
@@ -14,8 +8,6 @@ M.setup = function()
 			metadata["injection.language"] = ext
 		end
 	end, {})
-	-- Setup injection for gotmpl filetypes
-	vim.treesitter.query.set("gotmpl", "injections", DEFAULT_INJECTION)
 	-- Make sure vim recognizes .tmpl files as gotmpl ft
 	vim.filetype.add({
 		extension = {
