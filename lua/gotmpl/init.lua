0f01eab85954930526b75db713ebfd6ea80ef848
Pavel Dydyshka <paul.dydyshko@gmail.com>
2026-03-22 16:38:35 +0100

    fix: let ext regex match numerics as well
    This enables support of extensions like .ps1 or whatever contains not
    only alphabetical characters

--- lua/gotmpl/init.lua
+++ lua/gotmpl/init.lua
@@ -3,7 +3,7 @@ local M = {}
 M.setup = function()
 	vim.treesitter.query.add_directive("inject-go-tmpl!", function(_, _, bufnr, _, metadata)
 		local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
-		local _, _, ext, _ = string.find(fname, ".*%.(%a+)(%.%a+)")
+		local _, _, ext, _ = string.find(fname, ".*%.([^.]+)(%.%a+)")
 		if ext then
 			metadata["injection.language"] = ext
 		end
